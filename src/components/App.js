70296d65383c54c3759ed2fa9b5dd237c88203fa
Nick Bechtel <bechtel.nick@gmail.com>
2021-11-11 19:28:16 -0500

    commit tree d722c30377766170371ab22ee510509936c68942

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,18 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogs, setDogs] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((r) => r.json())
+      .then((data) => setDogs(data.message));
+  }, []);
+
+  if (!dogs) return <p>Loading</p>;
+
+  return <img src={dogs} alt="A Random Dog" />;
+}
+
+export default App;
